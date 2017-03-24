175888c15f4d5f8d2b97558e649507de128967f5
kannanmurthy <kannansm10@gmail.com>
2017-03-24 12:59:57 -0700

    Made knight class and added a few methods

--- src/Knight.java
+++ src/Knight.java
@@ -1,5 +1,38 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by kannanmurthy on 3/24/17.
  */
 public class Knight {
+    private BufferedImage knight;
+    private int col;
+    private int row;
+
+    public Knight(int row, int col) {
+        setUpImg();
+        this.col = col;
+        this.row = row;
+    }
+
+    private void setUpImg() {
+        try{
+            knight = ImageIO.read(new File("knight.png"));
+        } catch (IOException e) {
+            System.out.println("Couldn't open image!");
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(knight,col,row,null);
+    }
+
+
+
+
+
 }
