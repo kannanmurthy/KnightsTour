c7b86ff4cba6bba585559e0cd67e2e28a45e21e3
kannanmurthy <kannansm10@gmail.com>
2017-03-24 12:50:06 -0700

    Initialized 2D array of possible moves

---
+++ src/Knight.java
@@ -0,0 +1,5 @@
+/**
+ * Created by kannanmurthy on 3/24/17.
+ */
+public class Knight {
+}
--- src/KnightsTourPanel.java
+++ src/KnightsTourPanel.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
@@ -12,25 +14,34 @@ import javax.swing.JPanel;
  * always should.
  */
 
-public class KnightsTourPanel extends JPanel {
+public class KnightsTourPanel extends JPanel implements MouseListener {
 
 	// what private data is needed?
+	private boolean clicked = false;
+	public int[][] knightGrid = {{2,3,4,4,4,4,3,2},
+			{3,4,6,6,6,6,4,3},
+			{4,6,8,8,8,8,6,4},
+			{4,6,8,8,8,8,6,4},
+			{4,6,8,8,8,8,6,4},
+			{4,6,8,8,8,8,6,4},
+			{3,4,6,6,6,6,4,3},
+			{2,3,4,4,4,4,3,2}
+	};
 
 
 	public KnightsTourPanel(int w, int h) {
 		this.setPreferredSize(new Dimension(w,h));
 		this.setBackground(Color.green);
-		addMouseListener();
+		addMouseListener(this);
 	}
 
 	// add the mouse listener.  This will only work for the 
 	// first click, and then after the first click, there should
 	// be no more mouse listening!
-	private void addMouseListener() {
-		
+
+	private static void addMouseListener() {
+
 	}
-
-
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		// stuff to draw the board and knight
@@ -54,6 +65,37 @@ public class KnightsTourPanel extends JPanel {
 
 		return false;
 	}
-	
-	
+
+	public void reachableDecrement() {
+
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(clicked == false) {
+			System.out.println("Clicked! " + e.getButton());
+			clicked = true;
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
 }
