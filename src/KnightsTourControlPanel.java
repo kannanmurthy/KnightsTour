d8ba741358cffd0d5ad0749e65af71e200964ea5
kannanmurthy <kannansm10@gmail.com>
2017-03-29 12:30:56 -0700

    Two versions of the project merged

--- src/KnightsTourControlPanel.java
+++ src/KnightsTourControlPanel.java
@@ -1,18 +1,35 @@
+//<<<<<<< HEAD
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 /* One of the tricky things you have to figure out is how to have
  * the controls in the control panel talk to the KnightsTourPanel.
  * I know you'll figure out a way.  DON'T USE STATIC METHODS!!!!!
  */
-
 public class KnightsTourControlPanel extends JPanel {
-	
+
+	int WIDTH;
+	int BUTTON_WIDTH;
+	int BUTTON_HEIGHT;
+	int BUTTON_Y;
+	int INCREMENT;
+
 	public KnightsTourControlPanel(int w, int h) {
+		WIDTH = w;
+		BUTTON_WIDTH = 140;
+		BUTTON_HEIGHT = 50;
+		BUTTON_Y = 80;
+		INCREMENT = (WIDTH - (4*BUTTON_WIDTH))/5;
 		this.setPreferredSize(new Dimension(w, h));
-		this.setBackground(Color.orange);
+		this.setBackground(new Color(51, 102, 255));
 		setUpButtonsAndSliders();
 	}
+
 
 	/* Add all the buttons and sliders used to control this Knight's tour.
 	 * It is best if you allow the placement of the components to be handled
@@ -20,6 +37,127 @@ public class KnightsTourControlPanel extends JPanel {
 	 * it!  You can also bind key events to the buttons and sliders, as well
 	 */
 	private void setUpButtonsAndSliders() {
-		
+
+		int MIN = 0;
+		int MAX = 100;
+		int INTERVAL = 15;
+
+		JLabel speedLabel = new JLabel("Speed"); //i cant get this label to show up on the panel
+		this.setLayout(null);
+		this.add(speedLabel);
+		speedLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+		speedLabel.setLocation(10, 10);
+
+		JButton start = new JButton("Start");
+		this.add(start);
+		start.setBounds(INCREMENT, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT );
+		start.setFont(new Font("Arial", Font.PLAIN, 30));
+		start.setFocusPainted(false);
+		start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getKnightsTourPanel(new KnightsTourPanel(600, 800));
+				repaint();
+			}
+
+			private void getKnightsTourPanel(KnightsTourPanel tp) {
+				tp.start();
+			}
+		});
+
+		JButton pause = new JButton("Pause");
+		this.add(pause);
+		pause.setBounds((2*INCREMENT + BUTTON_WIDTH), BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT );
+		pause.setFont(new Font("Arial", Font.PLAIN, 30));
+		pause.setFocusPainted(false);
+		pause.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getKnightsTourPanel(new KnightsTourPanel(600, 800));
+				repaint();
+			}
+
+			private void getKnightsTourPanel(KnightsTourPanel tp) {
+				tp.pause();
+			}
+		});
+
+
+		JButton optimize = new JButton("Optimize");
+		this.add(optimize);
+		optimize.setBounds(3*INCREMENT + 2*BUTTON_WIDTH, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT );
+		optimize.setFont(new Font("Arial", Font.PLAIN, 30));
+		optimize.setFocusPainted(false);
+		optimize.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getKnightsTourPanel(new KnightsTourPanel(600, 800));
+				repaint();
+			}
+
+			private void getKnightsTourPanel(KnightsTourPanel tp) {
+				tp.makeThoughtfulMove();
+			}
+		});
+
+		JButton random = new JButton("Random");
+		this.add(random);
+		random.setBounds(4*INCREMENT + 3*BUTTON_WIDTH, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT );
+		random.setFont(new Font("Arial", Font.PLAIN, 30));
+		random.setFocusPainted(false);
+		random.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getKnightsTourPanel(new KnightsTourPanel(600, 800));
+				repaint();
+			}
+
+			private void getKnightsTourPanel(KnightsTourPanel tp) {
+				tp.makeRandomMove();
+			}
+		});
+
+		JSlider speedSlider = new JSlider(JSlider.HORIZONTAL,
+				MIN, MAX, INTERVAL);
+		speedSlider.setMajorTickSpacing(10);
+		speedSlider.setPaintTicks(true);
+		speedSlider.setPaintLabels(true);
+		this.add(speedSlider);
+		speedSlider.setBounds(260, 10, 280, 40);
+
+
+
 	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+
 }
+//=======
+//import java.awt.*;
+//
+//import javax.swing.JPanel;
+///* One of the tricky things you have to figure out is how to have
+// * the controls in the control panel talk to the KnightsTourPanel.
+// * I know you'll figure out a way.  DON'T USE STATIC METHODS!!!!!
+// */
+//
+//public class KnightsTourControlPanel extends JPanel {
+//
+//	public KnightsTourControlPanel(int w, int h) {
+//		this.setPreferredSize(new Dimension(w, h));
+//		this.setBackground(Color.orange);
+//		setUpButtonsAndSliders();
+//	}
+//
+//	/* Add all the buttons and sliders used to control this Knight's tour.
+//	 * It is best if you allow the placement of the components to be handled
+//	 * by a layout manager.  You can find out lots about layouts if you google
+//	 * it!  You can also bind key events to the buttons and sliders, as well
+//	 */
+//	private void setUpButtonsAndSliders() {
+//
+//	}
+//}
+//>>>>>>> a95284cdb57482ba4f1e1f29f3a7fc127f19ad1a
--- src/KnightsTourPanel.java
+++ src/KnightsTourPanel.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /*
  * This class will be the display and will get the starting position
@@ -17,6 +19,12 @@ import javax.swing.JPanel;
 public class KnightsTourPanel extends JPanel implements MouseListener {
 
 	// what private data is needed?
+	int WIDTH;
+	int HEIGHT;
+	int GRID_WIDTH;
+	int GRID_HEIGHT;
+	private Timer t;
+	Dimension d;
 	private boolean clicked = false;
 	public int[][] knightGrid = {{2,3,4,4,4,4,3,2},
 			{3,4,6,6,6,6,4,3},
@@ -30,9 +38,16 @@ public class KnightsTourPanel extends JPanel implements MouseListener {
 
 
 	public KnightsTourPanel(int w, int h) {
-		this.setPreferredSize(new Dimension(w,h));
-		this.setBackground(Color.green);
-		addMouseListener(this);
+		d = new Dimension(w,h);
+		this.setPreferredSize(d);
+		this.setBackground(new Color(150,150,150));
+		knightGrid = new int[8][8];
+		WIDTH = w;
+		HEIGHT = h;
+		GRID_WIDTH = WIDTH/knightGrid.length;
+		GRID_HEIGHT = HEIGHT/knightGrid[0].length;
+		addMouseListener();
+//		t.start();
 	}
 
 	// add the mouse listener.  This will only work for the 
@@ -42,10 +57,26 @@ public class KnightsTourPanel extends JPanel implements MouseListener {
 	private static void addMouseListener() {
 
 	}
+	private void makePanel() {
+		t = new Timer(10, new ActionListener() {// fires off every 10 ms
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				this.tick();
+				repaint();// naturally, we want to see the new view
+			}
+			private void tick() {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		// stuff to draw the board and knight
-
+		for(int r = 0; r < knightGrid.length; r++){
+			for(int c = 0; c< knightGrid[0].length;c++) {
+				g.drawRect(r*GRID_WIDTH, c*GRID_HEIGHT, GRID_WIDTH, GRID_HEIGHT);
+			}
+		}
 	}
 	/* make random move just selects a new location at random
 	 * if the knight is trapped (no new locations to move to)
@@ -53,7 +84,13 @@ public class KnightsTourPanel extends JPanel implements MouseListener {
 	 * The knight's location should be updated and the 
 	 */
 	public boolean makeRandomMove() {
-
+		System.out.println("Making random moves");
+		int row = (WIDTH/GRID_WIDTH);
+		int col = (HEIGHT/GRID_HEIGHT);
+		int randr = (int) (row*Math.random());
+		int randC = (int) (col*Math.random());
+		knightGrid[randr][randC]= 0;
+		// this only works for one time lol, it doesnt take into repeats and stuff so you might wanna rewrite this again or i can do it tomorrow in class
 		return false;
 	}
 	/* make a move to a new location that ensures the best chance
@@ -97,5 +134,13 @@ public class KnightsTourPanel extends JPanel implements MouseListener {
 	@Override
 	public void mouseExited(MouseEvent e) {
 
+	}
+
+	public void pause() {
+		System.out.println("Simulation Paused");
+	}
+
+	public void start() {
+		System.out.println("Simulation Started");
 	}
 }
